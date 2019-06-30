233f87c0e52e0f32e720c36c76fbfbf076a9a282
20171105122 <40412860+20171105122@users.noreply.github.com>
2019-06-30 18:10:23 +0800

    Update 未命名1.cpp

--- 未命名1.cpp
+++ 未命名1.cpp
@@ -122,8 +122,23 @@ int main()
 	    min=atoi(num[i-1][l-6]);
 	average[i-1]=(average[i-1]-max-min)/(l-7);
 	    fclose(fp);
-	
-return 0;
+	else{
+	    if(ch==',')
+		{
+			num[i-1][l-6][m+1]='\0';
+			average[i-1]+=atof(num[i-1][l-6]);
+			if(max<atoi(num[i-1][l-6]))
+			    max=atoi(num[i-1][l-6]);
+			    
+			if(min>atoi(num[i-1][l-6]))
+				min=atoi(num[i-1][l-6]);
+				l++;
+				m=0;
+				ch=fgetc(fp);
+				continue;
+        }
+    } 
+    return 0;
 }
 	    
  
